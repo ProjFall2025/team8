c435a9c249d728a92cfa9187f5b0d71f869e9ece
Vishal <vp1412003@gmail.com>
2025-12-01 03:08:44 -0500

    Cleaned leaseTenants.js merge markers and added landlord tenants endpoint

--- backend/routes/leaseTenants.js
+++ backend/routes/leaseTenants.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const leaseTenantController = require('../controllers/leaseTenantController');
 const auth = require('../middlewares/auth');
 
-<<<<<<< HEAD
 // Get all lease–tenant relationships (admin use)
 router.get('/', auth, leaseTenantController.getAll);
 
@@ -20,12 +19,6 @@ router.get('/landlord-tenants/:landlordId', auth, leaseTenantController.getByLan
 router.post('/', auth, leaseTenantController.addTenant);
 
 // Remove tenant from a lease
-=======
-router.get('/', auth, leaseTenantController.getAll);
-router.get('/lease/:leaseId', auth, leaseTenantController.getByLease);
-router.get('/user/:userId', auth, leaseTenantController.getByUser);
-router.post('/', auth, leaseTenantController.addTenant);
->>>>>>> 1cff3b005ec95393bd523a7d6f77e9d0c64425d0
 router.delete('/', auth, leaseTenantController.removeTenant);
 
 module.exports = router;
