74c43e9373c41973bcd0d7c3e01754667beae511
Vishal <vp1412003@gmail.com>
2025-12-01 02:56:29 -0500

    Added leaseArchive routes with get, create, and delete endpoints

--- backend/routes/leaseArchive.js
+++ backend/routes/leaseArchive.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const leaseArchiveController = require('../controllers/leaseArchiveController');
 const auth = require('../middlewares/auth');
 
+// Lease archive routes
 router.get('/', auth, leaseArchiveController.getAll);
 router.get('/:leaseId', auth, leaseArchiveController.getByLease);
 router.post('/', auth, leaseArchiveController.create);
