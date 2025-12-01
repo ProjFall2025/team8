f66de772a8ee5aad74913320ab160629c205ff46
Vishal <vp1412003@gmail.com>
2025-12-01 03:11:24 -0500

    Cleaned smartPasscodes.js merge markers and added validation, regeneration, revoke, and users-with-leases route

--- backend/routes/smartPasscodes.js
+++ backend/routes/smartPasscodes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 
 const smartPasscodeController = require('../controllers/smartPasscodeController');
 const auth = require('../middlewares/auth');
@@ -42,7 +41,6 @@ router.post('/validate', auth, smartPasscodeController.validate);
 // Regenerate Passcode
 router.post('/regenerate', auth, smartPasscodeController.regenerate);
 
-
 /**
  * ---------------------------------------------------------------------
  * PARAMETER ROUTES (Must always be last)
@@ -61,15 +59,4 @@ router.delete('/:passcode_id', auth, smartPasscodeController.delete);
 // Revoke passcode by ID
 router.patch('/:passcode_id/revoke', auth, smartPasscodeController.revoke);
 
-
 module.exports = router;
-=======
-const smartPasscodeController = require('../controllers/smartPasscodeController');
-const auth = require('../middlewares/auth');
-
-router.get('/:lease_id', auth, smartPasscodeController.getByLease);
-router.post('/', auth, smartPasscodeController.create);
-router.delete('/:passcode_id', auth, smartPasscodeController.delete);
-
-module.exports = router;
->>>>>>> 1cff3b005ec95393bd523a7d6f77e9d0c64425d0
